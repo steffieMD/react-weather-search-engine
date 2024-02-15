55a022156ab18fe8c4eb7ed7b43f5e3d39830e73
Stephanie Metieh <stephaniemetieh@gmail.com>
2024-02-15 10:50:51 +0100

    added bootstrap

--- src/Footer.js
+++ src/Footer.js
@@ -4,8 +4,10 @@ export default function Footer() {
   return (
     <p>
       This project was coded by <a href="/">Steff Metieh</a> and is{" "}
-      <a href="/">open-sourced on GitHub</a> and hosted on{" "}
-      <a href="/">Netlify</a>
+      <a href="https://github.com/steffieMD/react-weather-search-engine">
+        open-sourced on GitHub
+      </a>{" "}
+      and hosted on <a href="/">Netlify</a>
     </p>
   );
 }
