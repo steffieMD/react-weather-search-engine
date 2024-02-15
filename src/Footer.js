854e7145cfd4c5d392c5d89ad4f99ff241f8e730
Stephanie Metieh <stephaniemetieh@gmail.com>
2024-02-15 10:08:52 +0100

    created a react weather search engine

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <p>
+      This project was coded by <a href="/">Steff Metieh</a> and is{" "}
+      <a href="/">open-sourced on GitHub</a> and hosted on{" "}
+      <a href="/">Netlify</a>
+    </p>
+  );
+}
