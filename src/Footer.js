8a8186bad7c5ee82323d6dd31fd9ac4b916f8657
Stephanie Metieh <stephaniemetieh@gmail.com>
2024-02-25 09:15:44 +0100

    added date

--- src/Footer.js
+++ src/Footer.js
@@ -4,11 +4,21 @@ import "./App.css";
 export default function Footer() {
   return (
     <p className="footer">
-      This project was coded by <a href="/">Steff Metieh</a> and is{" "}
-      <a href="https://github.com/steffieMD/react-weather-search-engine">
+      This project was coded by{" "}
+      <a href="/" target="blank">
+        Steff Metieh
+      </a>{" "}
+      and is{" "}
+      <a
+        href="https://github.com/steffieMD/react-weather-search-engine"
+        target="blank"
+      >
         open-sourced on GitHub
       </a>{" "}
-      and hosted on <a href="/">Netlify</a>
+      and hosted on{" "}
+      <a href="/" target="blank">
+        Netlify
+      </a>
     </p>
   );
 }
